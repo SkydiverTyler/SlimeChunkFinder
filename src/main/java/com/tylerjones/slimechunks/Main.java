cbac43071c7a6e808f65ebbf3dabc25b33ceb090
Tyler <skydivertyler@gmail.com>
2020-02-07 11:08:37 -0500

    refactoring and cleaning up

--- src/main/java/com/tylerjones/slimechunks/Main.java
+++ src/main/java/com/tylerjones/slimechunks/Main.java
@@ -1,10 +1,10 @@
 /*
  * Author: Tyler Jones
- * Course: CITP 190
- * Project: 
- * Summary: 
+ * Date: Feb. 7, 2020
+ * Project: Slime Chunk Finder
+ * Summary: A tool to find Minecraft Slime Chunks
  */
-package com.tylerjones.seedfindertest;
+package com.tylerjones.slimechunks;
 import java.util.*;
 
 public class Main {
@@ -18,25 +18,24 @@ public class Main {
         final int MAX_CHUNK_NUM = Integer.MAX_VALUE/16;
         
 //        y is first [] each line, x is second [] each sub-element
-//        boolean[][] pattern =
-//        {
-//            {true, true, true, true, true, true, true, true}
-//        };
-//        
-//        int xp = 272;
-//        int zp = 272;
-//        
-//        System.out.println(xp + ",\t" + zp + ",\t" + isSlime(seed, xp, zp));
+        boolean[][] pattern =
+        {
+            {true, true, true, true, true, true, true, true}
+        };
         
-        System.out.println(MAX_CHUNK_NUM);
-
-        for(int i=0; i<MAX_CHUNK_NUM; i++) {
-//            System.out.println(i);
-        }
-
+//        test chunk inputs
+        int xp = 259;
+        int zp = 432;
+        
+        System.out.println(xp + ",\t" + zp + ",\t" + isSlime(seed, xp, zp));
+        
+//        System.out.println(MAX_CHUNK_NUM);
+//        for(int i=0; i<MAX_CHUNK_NUM; i++) {
+////            System.out.println(i);
+//        }
 
 //        keep the program open
-//        sc.nextLine();
+        sc.nextLine();
         
     }
     
@@ -51,6 +50,6 @@ public class Main {
         );
         return rnd.nextInt(10) == 0;
         
-    }
+    }        
     
 }
